cec55a20f8bc005bfee6a4a596ec53378d90788a
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-01-23 22:48:42 +0300

    u8

--- academy/src/by/academy/HomeWork/HW1/DegreeOfNumber2.java
+++ academy/src/by/academy/HomeWork/HW1/DegreeOfNumber2.java
@@ -5,10 +5,10 @@ public class DegreeOfNumber2 {
 	public static void main(String[] args) {
 
 		for (int i = 0; Math.pow(2, i) <= 1_000_000; i++) {
-			System.out.println("2 v stepeni " + i + " = " + Math.pow(2, i));
+			System.out.println(" 2 v stepeni " + i + " = " + Math.pow(2, i));
 
 		}
-
+		// Найти произведение двузначных нечетных чисел кратных
 	}
 
 }
--- academy/src/by/academy/classwork/lesson4/Task12.java
+++ academy/src/by/academy/classwork/lesson4/Task12.java
@@ -3,18 +3,16 @@ package by.academy.classwork.lesson4;
 public class Task12 {
 
 	public static void main(String[] args) {
-		//12. Найти произведение двузначных нечетных чисел кратных 13.
-int pr;
-		for (int i=10; i<=99; i++) {
-			
-			for (int i=10; i<=99; i++)
+		// 12. Найти произведение двузначных нечетных чисел кратных 13.
+		int pr = 1;
+		for (int i = 10; i <= 99; i++) {
 
-			if (i%1==1 && i%13==0) {
-			pr=1
+			if (i % 1 == 1 && i % 13 == 0) {
+				pr = pr * i;
 			}
-		
+			System.out.println(pr);
 		}
-		System.out.println(pr);
+
 	}
 
 }
