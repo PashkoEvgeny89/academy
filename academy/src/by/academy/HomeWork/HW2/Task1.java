63ecd5899419abd1283d09d9b07615c0c768b299
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-01-24 14:39:46 +0300

    test_void

--- academy/src/by/academy/HomeWork/HW2/Task1.java
+++ academy/src/by/academy/HomeWork/HW2/Task1.java
@@ -1,13 +1,23 @@
 package by.academy.HomeWork.HW2;
 
 public class Task1 {
+	 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hello");
-System.out.println("�������!!!");
-System.out.println("Привет utf8");
+
+cat("nam-nam-nam", "Hello");
 
 	}
 
+	static int raz(int n) {
+		int y;
+		y=n*3;
+		return y;
+	}
+	
+	static void cat(String s, String p) {
+		System.out.println("Kotik delaet "+s+" "+raz(3) +" raz i govorit "+p);
+	}
+	
+	
 }
