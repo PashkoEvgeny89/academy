f133bc80ebc88a59f50336063672f23e000732a7
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-01-28 22:20:11 +0300

    task1

--- academy/src/by/academy/HomeWork/HW2/Task1.java
+++ academy/src/by/academy/HomeWork/HW2/Task1.java
@@ -1,33 +1,41 @@
 package by.academy.HomeWork.HW2;
 
+import java.util.Scanner;
+
 public class Task1 {
 
 	public static void main(String[] args) {
 		
-		int n=17810393;
-		int sec;
-		int min;
-		int hour;
-		int day;
-		int month;
-		int year;
-		
-		sec=n%60;
-		int p;
-		p=(n-sec)/60;
-		min=p%60;
-		p=(p-min)/60;
-		hour=p%24;
-		p=(p-hour)/24;
-		day=p%30;
-		p=(p-day)/30;
-		month=p%12;
-		year=p-month;
-		
-		System.out.println(" year:"+year+" month:"+month+" day:"+day+" hour:"+hour+" min:"+min+" sec:"+sec);
-		int proverka;
-		proverka=sec+min*60+hour*60*60+day*60*60*24+month*60*60*24*30+year*60*60*24*30*12;
-		System.out.println(proverka);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 lines:");
+		String str1 = sc.nextLine();
+		String str2 = sc.nextLine();
+		sc.close();
 
+		StringBuffer str11 = new StringBuffer(str1);
+		StringBuffer str22 = new StringBuffer(str2);
+
+		if (str1.length() == str2.length()) {
+
+			for (int i = 0; i < str1.length(); i++) {
+				for (int q = 0; q < str11.length(); q++) {
+					if (str11.charAt(0) == str22.charAt(q)) {
+						str11 = str11.deleteCharAt(0);
+						str22 = str22.deleteCharAt(q);
+						System.out.println(str11);
+						System.out.println(str22);
+					}
+				}
+			}
+
+		}
+		
+		if (str11.length()==0) {
+			System.out.println("true");
+			
+		}
+		else {
+			System.out.println("false");
+		}
 	}
 }
