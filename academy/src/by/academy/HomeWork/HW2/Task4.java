f181a9e05a7a65a2b7d82e4dcf10135db6555dde
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-01-30 02:46:09 +0300

    task4_kiloda

--- academy/src/by/academy/HomeWork/HW2/Task4.java
+++ academy/src/by/academy/HomeWork/HW2/Task4.java
@@ -16,9 +16,27 @@ public class Task4 {
 		//.add добавляет элемент в массив
 		
 		ArrayList<String> koloda = new ArrayList<String>();
-		koloda.add("2 chirva");
+	//	koloda.add("2 chirva");
+		String[] masti={"_chervi","_bubna","_trevi","_piki"};
+		String[] big={"V","D","K","T"};
+		for (int q=0; q<masti.length; q++) {
+		for (int i=2; i<=10; i++) {
+			koloda.add(i+masti[q]);
+		}
+		for (int k=0; k<big.length; k++) {
+			koloda.add(big[k]+masti[q]);
+		}
+		
+		}
+		
+		
+		
 
+	//	System.out.println(koloda.get(0));
+		System.out.println(koloda.size());
 		System.out.println(koloda);
+		//koloda.remove(2);
+
 	}
 
 }
