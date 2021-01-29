30c19b4a9dcb119258a809c354d79fcd23900a2e
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-01-30 02:03:33 +0300

    02:03

---
+++ academy/src/by/academy/HomeWork/HW2/Task2.java
@@ -0,0 +1,51 @@
+package by.academy.HomeWork.HW2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		System.out.print("How many words do you want to enter?: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Enter " + n + " words:");
+
+		String[] array = new String[n];
+		sc.nextLine();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextLine();
+		}
+
+		HashMap<Integer, String> lengthword = new HashMap<>();
+		int[] key = new int[n];
+
+		for (int i = array.length - 1; i >= 0; i--) {
+			StringBuffer word = new StringBuffer(array[i]);
+
+			// removing duplicate characters from a word
+			for (int q = 0; q < word.length(); q++) {
+				int max = word.length();
+				for (int p = q + 1; p < max; max--) {
+					if (word.charAt(q) == word.charAt(max - 1)) {
+						word = word.deleteCharAt(max - 1);
+					}
+				}
+			}
+			// saving the number (Key) of different characters in a word and its meaning (Value)
+			lengthword.put(word.length(), array[i]);
+			key[i] = word.length();
+		}
+
+		Arrays.sort(key);
+
+		System.out.println(
+				"The first word in which the number of different characters is minimal: " + lengthword.get(key[0]));
+
+		sc.close();
+
+	}
+
+}
---
+++ academy/src/by/academy/HomeWork/HW2/Task4.java
@@ -0,0 +1,24 @@
+package by.academy.HomeWork.HW2;
+
+import java.util.ArrayList;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ 		Напишите программу Deal.java, 
+  		которая должна имитировать раздачу карт для игры в покер. 
+		Программа получает число n, задаваемое с консоли пользователем, 
+		и раздает карты на n  игроков (по 5 карт каждому) из рассортированной колоды. 
+		Разделяйте пять карт, выданных каждому игроку, пустой строкой.
+ */
+		//.add добавляет элемент в массив
+		
+		ArrayList<String> koloda = new ArrayList<String>();
+		koloda.add("2 chirva");
+
+		System.out.println(koloda);
+	}
+
+}
