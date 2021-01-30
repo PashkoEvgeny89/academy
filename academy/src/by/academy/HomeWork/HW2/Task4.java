a1e34779ae43db8ee3267d5ab8ba3115d00839ac
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-01-30 03:39:45 +0300

    task4

--- academy/src/by/academy/HomeWork/HW2/Task4.java
+++ academy/src/by/academy/HomeWork/HW2/Task4.java
@@ -1,41 +1,46 @@
 package by.academy.HomeWork.HW2;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Task4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-/*
- 		Напишите программу Deal.java, 
-  		которая должна имитировать раздачу карт для игры в покер. 
-		Программа получает число n, задаваемое с консоли пользователем, 
-		и раздает карты на n  игроков (по 5 карт каждому) из рассортированной колоды. 
-		Разделяйте пять карт, выданных каждому игроку, пустой строкой.
- */
-		//.add добавляет элемент в массив
-		
-		ArrayList<String> koloda = new ArrayList<String>();
-	//	koloda.add("2 chirva");
-		String[] masti={"_chervi","_bubna","_trevi","_piki"};
-		String[] big={"V","D","K","T"};
-		for (int q=0; q<masti.length; q++) {
-		for (int i=2; i<=10; i++) {
-			koloda.add(i+masti[q]);
-		}
-		for (int k=0; k<big.length; k++) {
-			koloda.add(big[k]+masti[q]);
-		}
-		
-		}
-		
-		
-		
 
-	//	System.out.println(koloda.get(0));
-		System.out.println(koloda.size());
-		System.out.println(koloda);
-		//koloda.remove(2);
+		System.out.print("How many players?: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+
+		ArrayList<String> koloda = new ArrayList<String>();
+
+		String[] masti = { "_chervi", "_bubna", "_trevi", "_piki" };
+		String[] big = { "V", "D", "K", "T" };
+		for (int q = 0; q < masti.length; q++) {
+			for (int i = 2; i <= 10; i++) {
+				koloda.add(i + masti[q]);
+			}
+			for (int k = 0; k < big.length; k++) {
+				koloda.add(big[k] + masti[q]);
+			}
+
+		}
+
+		String[][] igroki = new String[n][5];
+
+		Random rand = new Random();
+		for (int q = 0; q < n; q++) {
+		
+			for (int i = 0; i < 5; i++) {
+
+				igroki[q][i] = koloda.get(rand.nextInt(koloda.size() + 1));
+				System.out.print(igroki[q][i] + "  ");
+				koloda.remove(igroki[q][i]);
+			}
+			System.out.println();
+			System.out.println();
+		}
 
 	}
 
