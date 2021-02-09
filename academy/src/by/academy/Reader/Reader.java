e53ed6acf3a6c2cc85e0d34914d9be791e076c3e
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-02-10 00:27:09 +0300

    reader

--- academy/src/by/academy/Reader/Reader.java
+++ academy/src/by/academy/Reader/Reader.java
@@ -4,44 +4,43 @@ public class Reader {
 
 //	takeBook, который будет принимать переменное количество названий книг.
 //	Выводит на консоль сообщение "Петров В. В. взял книги: Приключения, Словарь, Энциклопедия"..
-	
-	
+
 	String name;
 	Integer numberReadCard;
 	String faculty;
 	String dateBirthday;
 	String numberPhone;
-	
+
 	Reader(String name, Integer numberReadCard, String faculty, String dateBirthday, String numberPhone) {
-		this.name=name;
-		this.numberReadCard=numberReadCard;
-		this.faculty=faculty;
-		this.dateBirthday=dateBirthday;
-		this.numberPhone=numberPhone;
+		this.name = name;
+		this.numberReadCard = numberReadCard;
+		this.faculty = faculty;
+		this.dateBirthday = dateBirthday;
+		this.numberPhone = numberPhone;
 	}
-	
-	Reader(){
+
+	Reader() {
 		super();
 	}
-	
-	public void nameBook(String... books){
+
+	public void nameBook(String... books) {
 		System.out.print("petrov v.v. vzjal books: ");
-	   for (String book: books) {
-	        System.out.print(book+", ");
+		for (String book : books) {
+			System.out.print(book + ", ");
+		}
 	}
-	}
-	
-	public void nameBookAndAvtorBook(Book... books){
+
+	public void nameBookAndAvtorBook(Book... books) {
 		System.out.print("petrov v.v. vzjal books: ");
-	   for (Book book: books) {
-	        System.out.print(book.getNameBooks()+", ");
+		for (Book book : books) {
+			System.out.print(book.getNameBooks() + ", ");
+		}
 	}
-	}
-	
+
 	public void takeBook(int n) {
-		System.out.println("petrov v.v. vzjal "+n+" book");
+		System.out.println("petrov v.v. vzjal " + n + " book");
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -83,9 +82,7 @@ public class Reader {
 	}
 
 	public void returnBook() {
-		
+
 	}
-	
-	
 
 }
