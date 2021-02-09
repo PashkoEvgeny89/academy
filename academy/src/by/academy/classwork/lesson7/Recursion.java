36ef43590b298c20e77d74c547b0d7269c724a05
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-02-10 01:04:16 +0300

    rec

---
+++ academy/src/by/academy/classwork/lesson7/Recursion.java
@@ -0,0 +1,33 @@
+package by.academy.classwork.lesson7;
+
+public class Recursion {
+
+	public static void rec(int a, int b) {
+		if (a < b) {
+			System.out.println(a);
+			a++;
+			rec(a, b);
+		} 
+		
+		if (b<a){
+			System.out.println(a);
+			a--;
+			rec(a, b);
+		}
+	}
+
+//	5.	Даны два целых числа A и В. Выведите все числа от A до B включительно,
+//	в порядке возрастания, если A < B, или в порядке убывания в противном случае. 
+//	Использовать рекурсию
+
+	public void recursionFucn() {
+		System.out.println("Привет, JavaRush!");
+		recursionFucn();
+	}
+
+	public static void main(String[] args) {
+
+		rec(9, 2);
+	}
+
+}
