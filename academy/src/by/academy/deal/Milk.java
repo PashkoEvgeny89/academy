5c084c8c68d3743c828124b616695bd3dfd0be77
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-02-14 00:37:02 +0300

    milk

--- academy/src/by/academy/deal/Milk.java
+++ academy/src/by/academy/deal/Milk.java
@@ -1,40 +1,15 @@
 package by.academy.deal;
 
 public class Milk extends Product {
-	
+
 	double percentFat;
-	
-	
-/*	private static int milkCount = 0;
-	private int nonStaticMilkCount = 0;
-	public static int getMilkCount() {
-		return milkCount;
-	}
 
-    public static void setMilkCount(int milkCount) {
-		Milk.milkCount = milkCount;
-	}
-
-	public int getNonStaticMilkCount() {
-		return nonStaticMilkCount;
-	}
-
-	public void setNonStaticMilkCount(int nonStaticMilkCount) {
-		this.nonStaticMilkCount = nonStaticMilkCount;
-	}
-
-	*/
-	
-	
 	public Milk(String type, double price, String manufacturer, int quantity, double percentFat) {
 		super(type, price, manufacturer, quantity);
-		this.percentFat=percentFat;
-		
-	//	milkCount++;            //
-	//	nonStaticMilkCount++;       //
+		this.percentFat = percentFat;
 	}
-	
-	Milk(){
+
+	Milk() {
 		super();
 	}
 
@@ -48,12 +23,11 @@ public class Milk extends Product {
 
 	@Override
 	public double discount() {
-			if (quantity > 20) {
-				return 0.9;
-			} else {
-				return 1.0;
-			}
+		if (quantity > 20) {
+			return 0.9;
+		} else {
+			return 1.0;
 		}
-	
-	
+	}
+
 }
