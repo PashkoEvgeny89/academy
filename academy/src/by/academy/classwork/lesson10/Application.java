49e90ebe8308a232ea2b1c24fd4fb001caad10fa
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-02-13 13:39:13 +0300

    enum

---
+++ academy/src/by/academy/classwork/lesson10/Application.java
@@ -0,0 +1,37 @@
+package by.academy.classwork.lesson10;
+
+public class Application {
+
+	public static void main(String[] args) {
+		DayOfWeek dayofweek=DayOfWeek.Monday;
+		
+		for (DayOfWeek c: DayOfWeek.values()) {
+			System.out.println(c);
+		}
+
+		switch (dayofweek) {
+		case Monday:
+			System.out.println("Today Monday ");
+			break;
+		case Tuesday:
+			System.out.println("Today Tuesday ");
+			break;
+		case Wednesday:
+			System.out.println("Today Wednesday ");
+			break;
+		case Thursday:
+			System.out.println("Today Thursday ");
+			break;
+		case Friday:
+			System.out.println("Today Friday ");
+			break;
+		case Saturday:
+			System.out.println("Today Saturday ");
+			break;
+		case Sunday:
+			System.out.println("Today Sunday ");
+			break;  
+		}
+	}
+
+}
---
+++ academy/src/by/academy/classwork/lesson10/DayOfWeek.java
@@ -0,0 +1,5 @@
+package by.academy.classwork.lesson10;
+
+public enum DayOfWeek {
+	Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
