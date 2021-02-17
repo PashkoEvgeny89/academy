d29c7bc6008bbda77e8c7054d2e10194e43b3784
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-02-17 22:27:26 +0300

    good

---
+++ academy/src/by/academy/classwork/lesson11/Task66.java
@@ -0,0 +1,10 @@
+package by.academy.classwork.lesson11;
+
+public class Task66 {
+
+	public static void main(String[] args) {
+		Task6<Integer, Double, Long> var = new Task6<>(2, 2.2, 6l);
+		var.name();
+	}
+
+}
