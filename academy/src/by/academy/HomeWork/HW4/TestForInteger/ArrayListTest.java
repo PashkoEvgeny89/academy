c0f5a6e902fb7d643ca996e0ee33b54ed453b467
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-02-22 01:14:51 +0300

    8 voids +

--- academy/src/by/academy/HomeWork/HW4/TestForInteger/ArrayListTest.java
+++ academy/src/by/academy/HomeWork/HW4/TestForInteger/ArrayListTest.java
@@ -13,139 +13,149 @@ public class ArrayListTest {
 
 		System.out.println(Arrays.toString(arr));
 
-		System.out.println(arr[arr.length - 1] != null);
+		// добавления в наш массив объекта типа T - add(T obj)
+//1.1 метод добавления в конец и увеличения при необходимости размера массива
+		// addElementToLast()
+		int valueElementLast = 3;
 
-//метод добавления в конец и увеличения при необходимости размера массива
-		// addToEnd()
-
+		if (arr[arr.length - 1] != null) {
+			Integer[] arr2 = new Integer[arr.length * 2 + 1];
+			System.arraycopy(arr, 0, arr2, 0, arr.length);
+			arr = arr2;
+		}
 		for (int i = 0; i <= arr.length; i++) {
-			if (arr[arr.length - 1] != null) {
-				Integer[] arr2 = new Integer[arr.length * 2 + 1];
-				System.arraycopy(arr, 0, arr2, 0, arr.length);
-				arr = arr2;
-			}
 			if (arr[i] == null) {
-				arr[i] = 3;
+				arr[i] = valueElementLast;
 				break;
 			}
 		}
-		System.out.println(arr.length);
 		System.out.println(Arrays.toString(arr));
 
-		for (int i = 0; i <= arr.length; i++) {
-			if (arr[arr.length - 1] != null) {
-				Integer[] arr2 = new Integer[arr.length * 2 + 1];
-				System.arraycopy(arr, 0, arr2, 0, arr.length);
-				arr = arr2;
-			}
-			if (arr[i] == null) {
-				arr[i] = 4;
-				break;
-			}
-		}
-		System.out.println(arr.length);
-		System.out.println(Arrays.toString(arr));
+		// 1.2 метод добавления в начало и увеличения при необходимости размера массива
+		// addElementToFirst()
 
-		for (int i = 0; i <= arr.length; i++) {
-			if (arr[arr.length - 1] != null) {
-				Integer[] arr2 = new Integer[arr.length * 2 + 1];
-				System.arraycopy(arr, 0, arr2, 0, arr.length);
-				arr = arr2;
-			}
-			if (arr[i] == null) {
-				arr[i] = 5;
-				break;
-			}
-		}
-		System.out.println(arr.length);
-		System.out.println(Arrays.toString(arr));
-
-		for (int i = 0; i <= arr.length; i++) {
-			if (arr[arr.length - 1] != null) {
-				Integer[] arr2 = new Integer[arr.length * 2 + 1];
-				System.arraycopy(arr, 0, arr2, 0, arr.length);
-				arr = arr2;
-			}
-			if (arr[i] == null) {
-				arr[i] = 6;
-				break;
-			}
-		}
-		System.out.println(arr.length);
-		System.out.println(Arrays.toString(arr));
-
-		for (int i = 0; i <= arr.length; i++) {
-			if (arr[arr.length - 1] != null) {
-				Integer[] arr2 = new Integer[arr.length * 2 + 1];
-				System.arraycopy(arr, 0, arr2, 0, arr.length);
-				arr = arr2;
-			}
-			if (arr[i] == null) {
-				arr[i] = 7;
-				break;
-			}
-		}
-		System.out.println(arr.length);
-		System.out.println(Arrays.toString(arr));
-
-		// метод добавления в начало и увеличения при необходимости размера массива
-		// addToFirst()
-
-		
-		
-		
+		int valueElementFirst = 4;
 		if (arr[arr.length - 1] != null) {
 			Integer[] arr2 = new Integer[arr.length * 2 + 1];
 			System.arraycopy(arr, 0, arr2, 0, arr.length);
 			arr = arr2;
 		}
 
-		for (int i = arr.length-2; i >= 0; i--) {
-			if (arr[i]!=null) {
-				arr[i+1]=arr[i];
+		for (int i = arr.length - 2; i >= 0; i--) {
+			if (arr[i] != null) {
+				arr[i + 1] = arr[i];
 			}
 		}
-		arr[0]=8;
-		System.out.println(arr.length);
+		arr[0] = valueElementFirst;
+
 		System.out.println(Arrays.toString(arr));
-		
-		
-		
+
+		// 1.3 метод добавления по индексу и увеличения при необходимости размера
+		// массива
+		// addElementByIndex(int index=4)
+		int valueElementIndex = 5;
+		int indexValueElementIndex = 4;
 		if (arr[arr.length - 1] != null) {
 			Integer[] arr2 = new Integer[arr.length * 2 + 1];
 			System.arraycopy(arr, 0, arr2, 0, arr.length);
 			arr = arr2;
+		}
+		int index = indexValueElementIndex;
+		for (int i = arr.length - 2; i >= index; i--) {
+			if (arr[i] != null) {
+				arr[i + 1] = arr[i];
+			}
+		}
+		arr[index] = valueElementIndex;
+
+		System.out.println(Arrays.toString(arr));
+
+		// 2 Взятие лемента по индексу
+		// getElemenByIndex(int index)
+		index = 3;
+		int a = arr[index];
+		System.out.println(a);
+
+		// 3 взятие последнего элемента getLastElement()
+		int elemetLast = 0;
+		for (int i = arr.length - 1; i >= index; i--) {
+
+			if (arr[i] != null) {
+				elemetLast = arr[i];
+				break;
+			}
+		}
+		System.out.println(elemetLast);
+
+		// 4 взятие первого элемента getFirstElement()
+		int elementFirst = arr[0];
+		System.out.println(elementFirst);
+
+		// 5 вывод размера массива
+		// outputSize()
+		int sizeArray = arr.length;
+		System.out.println(sizeArray);
+
+		// 6 вывод индекса последнего заполненого элемента (не путать с размерностью)
+		// getLastIndex!=null()
+
+		int lastIndex = 0;
+		for (int i = arr.length - 1; i >= 0; i--) {
+
+			if (arr[i] != null) {
+				lastIndex = i;
+				break;
+			}
+		}
+		System.out.println(lastIndex);
+
+		// 7 удаление элемента по индексу removeElementByIndex(int i)
+		// нужно исрользовать 6 задание lastIndex
+
+		int lastIndex1 = 0;
+		for (int i = arr.length - 1; i >= 0; i--) {
+
+			if (arr[i] != null) {
+				lastIndex1 = i;
+				break;
+			}
 		}
 
-		for (int i = arr.length-2; i >= 0; i--) {
-			if (arr[i]!=null) {
-				arr[i+1]=arr[i];
+		int removeElemen = 3;
+		for (int i = removeElemen; i < lastIndex1; i++) {
+			arr[i] = arr[i + 1];
+
+		}
+		arr[lastIndex1] = null;
+		System.out.println(Arrays.toString(arr));
+
+		// 8 удаление элемента по значению removeElementByValue(T obj)
+		// нужно исрользовать 6 задание lastIndex
+
+		int lastIndex2 = 0;
+		for (int i = arr.length - 1; i >= 0; i--) {
+
+			if (arr[i] != null) {
+				lastIndex2 = i;
+				break;
 			}
 		}
-		arr[0]=9;
-		System.out.println(arr.length);
-		System.out.println(Arrays.toString(arr));
-		
-		// метод добавления индексу и увеличения при необходимости размера массива
-				// addToIndex(int index=5)
-		
-		if (arr[arr.length - 1] != null) {
-			Integer[] arr2 = new Integer[arr.length * 2 + 1];
-			System.arraycopy(arr, 0, arr2, 0, arr.length);
-			arr = arr2;
-		}
-		int index=5;
-		for (int i = arr.length-2; i >= index; i--) {
-			if (arr[i]!=null) {
-				arr[i+1]=arr[i];
+
+		int removeElementValue = 3;
+		int removeIndex = 0;
+		for (int i = 0; i <= lastIndex2; i++) {
+			if (arr[i] == removeElementValue) { // нужно будет применить сравнение объектов
+				removeIndex = i;
+				for (int q = removeIndex; q < lastIndex2; q++) {
+					arr[q] = arr[q + 1];
+				}
+				arr[lastIndex2] = null;
+				break;
 			}
 		}
-		arr[index]=10;
-		System.out.println(arr.length);
+
 		System.out.println(Arrays.toString(arr));
-		
-		
-		
+
 	}
 
 }
