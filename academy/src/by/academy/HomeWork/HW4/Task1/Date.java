58bbb919f484a1694fac4e7b10f5da961054122a
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-02-26 22:53:19 +0300

    date

---
+++ academy/src/by/academy/HomeWork/HW4/Task1/Date.java
@@ -0,0 +1,70 @@
+package by.academy.HomeWork.HW4.Task1;
+
+import java.time.LocalDate;
+
+
+
+public class Date {
+	
+	//LocalDate
+	
+	//Calendar calendar = new GregorianCalendar(2017, 0 , 25);
+	
+	private Date date;  //dd-mm-yyyy
+	
+	/* 
+	Создать объект класса Date, используя вложенные классы Год, Месяц, День. 
+Методы: задать дату, вывести на консоль день недели по заданной дате. 
+День недели представить в виде перечисления. Пишем свой ином
+
+Один из конструкторов - строка в формате dd-mm-yyyy, добавить валидацию для этой строки. 
+После валидации в конструкторе создаем объекты класса Year, Month, Day.
+
+
+	
+	
+	 
+
+	if Validate true ->
+	Year  parse (4 dig)
+	Month  parse (2 big)
+	Day   parse (2 dig)     */
+	
+	
+	public Date getDate() {
+		return date;
+	}
+
+	
+	public void setDate1(Date date) {
+		this.date = date;
+	}
+	
+	public void printDate(Date date) {
+		System.out.println("Day of week=");
+		
+	}
+	
+	
+	
+		//Calendar calendar = new GregorianCalendar(new Date);
+		
+	
+
+		
+	
+	
+		
+	//int getDayOfYear();         ���������� ����� ��� � ������ ����
+
+		
+	
+	//	calendar.set(Calendar.YEAR, 2017); 			class Year parse date YEAR
+	//	calendar.set(Calendar.MONTH, 0);			class Month parse date MONTH
+	//	calendar.set(Calendar.DAY_OF_MONTH, 25);	class Day parse date DAY_OF_MONTH
+		
+		
+	
+	
+	
+}
