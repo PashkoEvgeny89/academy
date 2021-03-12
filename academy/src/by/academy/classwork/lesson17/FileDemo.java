0fab1a72039ad2215cbc432a05a09afe4f4c4ac9
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-03-13 00:26:13 +0300

    fileCopy

--- academy/src/by/academy/classwork/lesson17/FileDemo.java
+++ academy/src/by/academy/classwork/lesson17/FileDemo.java
@@ -6,12 +6,16 @@ public class FileDemo {
 
 		public static void main(String[] args) {
 
-			File file = new File("src/by/academy/classwork/lesson17");
+			File file = new File("src/by/academy/classwork/lesson17/file.txt");
+			
 
 			if (!file.exists()) {
 				file.mkdirs();
 			}
-		//	System.out.println(file.isAbsolute());
+			
+			System.out.println(file.isDirectory());
+			System.out.println(file.isAbsolute());
+			System.out.println(file.list());
 
 			for (File f : file.listFiles()) {
 				System.out.println("Имя файла: " + f.getName());
