6c45c10d7aaea914082b9f685fd709af90733ea2
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-03-15 01:03:32 +0300

    task1+

--- academy/src/by/academy/HomeWork/HW6/Task1/Task1.java
+++ academy/src/by/academy/HomeWork/HW6/Task1/Task1.java
@@ -1,30 +1,27 @@
 package by.academy.HomeWork.HW6.Task1;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task1 {
 
-		public static void main(String[] args) {
-			try (BufferedWriter bufferedWriter = new BufferedWriter(
-					new FileWriter("src\\by\\academy\\HomeWork\\HW6\\Task1\\fileTask1.txt"))) {
-				
-				
-				
-				String text = "stop1";
-				// писать код
-				
-				
-				
-				
+	public static final String FILE_PATH = "src\\by\\academy\\HomeWork\\HW6\\Task1\\fileTask1.txt";
+
+	public static void main(String[] args) {
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(FILE_PATH))) {
+
+			String text;
+			while (!"stop".equals(text = bufferedReader.readLine())) {
 				bufferedWriter.write(text);
-			} catch (IOException ex) {
-				System.out.println(ex.getMessage());
+				bufferedWriter.newLine();
 			}
+
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
 		}
 	}
-
-
-//Прочесть информацию введеную из клавиатури и записать в созданный вами txt файл, 
-//если введена "stop" строка тогда закончить запись в файл.
+}
