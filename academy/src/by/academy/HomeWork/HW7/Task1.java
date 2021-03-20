6edad0b50b5e814badea1ccc5df1214382cd3fe5
Evgeny <Evgeny@DESKTOP-LDDN99M>
2021-03-20 11:53:48 +0300

    hw7 task1 chernovik

--- academy/src/by/academy/HomeWork/HW7/Task1.java
+++ academy/src/by/academy/HomeWork/HW7/Task1.java
@@ -1,6 +1,7 @@
 package by.academy.HomeWork.HW7;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -10,14 +11,17 @@ public class Task1 {
 	public static final Random random = new Random();
 
 	public static void main(String[] args) {
-		ArrayList<Long> list = new ArrayList<>();
-		Random random = new Random();
-
+		ArrayList<Integer> list = new ArrayList<>();
+		
 		for (int i = 0; i < 100; i++) {
-			list.add(random.nextLong());
+			list.add(random.nextInt(100));
 		}
 
-		Map<Double, String> map = list.stream().sorted().map(x -> x * Math.PI - 20).distinct().filter(x->x<100)
+		System.out.println(list);
+		System.out.println(Collections.max(list));
+		System.out.println(Collections.min(list));
+
+		Map<Double, String> map = list.stream().sorted().map(x -> x * Math.PI - 20).distinct().filter(x -> x < 100)
 				.collect(Collectors.toMap(x -> x, x -> "Number: " + x));
 
 		System.out.println(map);
@@ -34,6 +38,5 @@ public class Task1 {
 //List<String> list = Stream.of(“One”, “Two”, “Three”).collect(Collectors.toList());
 
 //Не использовать циклы (for/while/do-while). Сгенерировать 100 рандомных Long чисел в диапазоне от 0 до 100. /
-//Каждое число умножить newна PI и отнять 20. Отфильтровать, оставив числа меньшие 100. Отсортировать по возрастанию. 
+// Отсортировать по возрастанию.
 //Пропустив первые 3 числа произвести следующие операции:
-//Преобразовать лист чисел в Map (операция collect), где ключем является само число а значением строка: ("Number: " + value).
